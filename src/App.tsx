3f55c5c02ddb3a50668560a561b84edc43175156
Evair Domingues <evairbd@gmail.com>
2025-04-14 11:14:53 -0300

    feat: components  DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './style/global.css';
 import { Container } from './components/Container';
 import { Menu } from './components/Menu';
 import { Logo } from './components/Logo';
-import { CountDown } from './components/CounDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -17,7 +17,29 @@ export function App() {
       </Container>
 
       <Container>
-        <CountDown />
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite aqui'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
