f77cfa0c9cbdc9a1c5910d9b057aaea689a2fabe
Evair Domingues <evairbd@gmail.com>
2025-04-17 11:02:57 -0300

    Feat: pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,7 @@
+import { Home } from './pages/Home';
 import './style/theme.css';
 import './style/global.css';
-import { Container } from './components/Container';
-import { Menu } from './components/Menu';
-import { Logo } from './components/Logo';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='task'
-              id='meuInput'
-              type='text'
-              placeholder='Digite aqui'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
-          </div>
-        </form>
-      </Container>
-    </>
-  );
+  return <Home />;
 }
